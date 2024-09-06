62a7bbb1f62c7591681577b362bc5c416c5d40ea
Arrowmaster <arrow@arrowmaster.net>
2024-09-05 23:21:43 -0400

    Add Vial support for atreus62

---
+++ keyboards/atreus62/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFC, 0x93, 0x0A, 0x1C, 0xBE, 0x15, 0x86, 0xA7}
+
+// Unlock combo is set to 'Escape' + 'Space'
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
